a42aa3709d33857fef1f2ea0f87e78e5eb6fe1b8
ruthnewman <ruthnewman@gmail.com>
2018-11-06 13:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
- const two = 2
+  const two = 2
   var n = n + two
   return n
 
-  // Feel free to move things around!
-  const two = 2
+  // Feel free to move things around 
 }
 
 var funkyFunction = function() {
